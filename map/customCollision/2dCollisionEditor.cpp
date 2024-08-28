84986bb9ceeff95a143aaad123fbb9fb9ed401ac
wijo2 <valo.keskevaari@gmail.com>
2024-08-28 12:18:56 +0300

    start on editor

--- map/customCollision/2dCollisionEditor.cpp
+++ map/customCollision/2dCollisionEditor.cpp
@@ -2,14 +2,31 @@
 
 class script 
 {
+	[text] QuadManager@ manager;
+
 	script() 
 	{
 		puts("2dCollisionEditor working c:");
+		if (@manager == null) 
+		{
+			@manager = @QuadManager(d2Math::IntRect(-2000, -2000, uint(2000), uint(2000)));
+		}
 	}
 }
 
+class QuadManager 
+{
+	[hidden] d2::CollisionManager@ manager;
+	[text] array<QuadEntity> quads;
 
-class CollisionEntity : trigger_base 
+	QuadManager() { @manager = @d2::CollisionManager(d2Math::IntRect()); }
+	QuadManager(d2Math::IntRect field) 
+	{
+		@manager = @d2::CollisionManager(field);
+	}
+}
+
+class QuadEntity : trigger_base 
 {
 	[position,mode:world,layer:19,y:p1y] float p1x;
 	[hidden] float p1y;
@@ -19,4 +36,21 @@ class CollisionEntity : trigger_base
 	[hidden] float p3y;
 	[position,mode:world,layer:19,y:p4y] float p4x;
 	[hidden] float p4y;
+
+	QuadManager@ quadManager;
+	d2::d2CQuad@ quad;
+
+	QuadEntity() {}
+
+	QuadEntity(QuadManager@ manager, uint colour) 
+	{ 
+		@this.quadManager = @manager; 
+		@quad = @d2::d2CQuad(
+		d2Math::Vector2(p1x, p1y),
+		d2Math::Vector2(p2x, p2y),
+		d2Math::Vector2(p3x, p3y),
+		d2Math::Vector2(p4x, p4y),
+		colour,
+		manager.manager);
+	}
 }
