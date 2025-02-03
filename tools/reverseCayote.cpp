95016e56cdb54eb81527a64ad34d01475d2131f6
valo keskevaari <valo.keskevaari@gmail.com>
2025-02-03 22:20:56 +0200

    why

--- tools/reverseCayote.cpp
+++ tools/reverseCayote.cpp
@@ -6,8 +6,9 @@ class script
 		controllable@ c = controller_entity(uint(get_active_player())).as_controllable();
 		int state = c.state();
 		dustman@ d = c.as_dustman();
-		if (state == 8 && crouchTimer < 4 && crouchTimer > 0 && d.dash() != 0) { d.dash(d.dash()-1); }
-		if (state == 10) { crouchTimer++; }
-		else { crouchTimer = 0; }
+		if (state == 8 && crouchTimer < 7 && crouchTimer > 0 && d.dash() != 0) { d.dash(d.dash()-1); }
+		else if (state == 10) { crouchTimer++; }
+		else if (state == 11 || state == 12 || state == 13) { crouchTimer = 7; }
+		else if (state != 11 && state != 12 && state != 13) { crouchTimer = 0; }
 	}
 }
