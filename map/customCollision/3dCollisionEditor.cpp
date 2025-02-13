567c2fa7d9db6246f09636ae36201ef45e63c889
valo keskevaari <valo.keskevaari@gmail.com>
2025-02-13 18:36:59 +0200

    text triggers

--- map/customCollision/3dCollisionEditor.cpp
+++ map/customCollision/3dCollisionEditor.cpp
@@ -1,5 +1,6 @@
 #include "3dNodeCluster.cpp";
 #include "3dFlats.cpp";
+#include "3dTriggers.cpp";
 //DustScripts/map/customCollision/3dCollisionEditor.cpp
 
 class script : script_base
@@ -32,10 +33,13 @@ class script : script_base
 	[hidden] float rotation;
 	float oldRotation;
 
+	//should I just have added a method list? yes.
+	//am I too lazy to do that at this point? yes.
 	array<d3NodeCluster@> nodeClusters;
 	array<FakeTrigger@> fakeTriggers;
 	array<d3FlatObjectBase@> flats;
 	array<d3EndFlag@> flags;
+	array<d3TextTrigger@> textTriggers;
 
 	d3StartPos@ startPos;
 	[hidden] Vector3 startCoords;
@@ -364,6 +368,10 @@ class script : script_base
 			{
 				flags[i].UpdateRotation();
 			}
+			for(uint i = 0; i < textTriggers.length(); i++)
+			{
+				textTriggers[i].UpdateRotation();
+			}
 			manager.UpdateLooks();
 		}
 	}
@@ -555,195 +563,6 @@ class script : script_base
 		for (int x = int(floor(-width/2.0)); x < int(floor(-width/2.0)) + lastRow; x += 1) 
 		{
 			s.set_tile(int(floor(pos.x/48))+x, int(floor(pos.y/48))+int(height/2), 19, t, false);
-		}
-	}
-}
-
-class d3StartPos : trigger_base
-{
-	scripttrigger@ self;
-	script@ script;
-	d3::d3Manager@ manager;
-
-	[hidden] d3FakeTrigger fakeTrigger;
-	[hidden] bool hasInit = false;
-
-	void init(script@ s, scripttrigger@ self)
-	{
-		@script = @s;
-		@manager = @s.manager;
-		@this.self = @self;
-
-		if (@s.startPos != null && !(s.startPos is this)&& !self.destroyed())
-		{
-			get_scene().remove_entity(s.startPos.self.as_entity());
-		}
-		if (!self.destroyed())
-		{
-			@s.startPos = @this;
-		}
-		if (!hasInit)
-		{
-			fakeTrigger = d3FakeTrigger();
-			fakeTrigger.colour = 0xFF00FF00;
-			hasInit = true;
-		}
-		fakeTrigger.Init(self.as_entity(), s, manager);
-		self.editor_handle_size(0);
-		UpdateRotation();
-		script.startCoords = fakeTrigger.pos;
-		s.firstFrame = true;
-	}
-
-	void editor_step()
-	{
-		fakeTrigger.EditorStep();
-		script.startCoords = fakeTrigger.pos;
-	}
-
-	void UpdateRotation()
-	{
-		fakeTrigger.UpdateRotation();
-	}
-
-	void on_remove()
-	{
-		fakeTrigger.DeleteSelf();
-	}
-}
-
-class d3EndFlag : trigger_base
-{
-	scripttrigger@ self;
-	script@ script;
-	d3::d3Manager@ manager;
-	input_api@ input;
-
-	[hidden] d3FakeTrigger fakeTrigger;
-	[hidden] bool hasInit = false;
-
-	//ids
-	[hidden] array<uint> attachedEntities;
-	array<entity@> entityHandles;
-
-	bool ended = false;
-
-	void init(script@ s, scripttrigger@ self)
-	{
-		@script = @s;
-		@manager = @s.manager;
-		@this.self = @self;
-		@input = @get_input_api();
-
-		if (!hasInit)
-		{
-			fakeTrigger = d3FakeTrigger();
-			fakeTrigger.colour = 0xFFFF0000;
-			hasInit = true;
-		}
-		fakeTrigger.Init(self.as_entity(), s, manager);
-		self.editor_handle_size(0);
-		UpdateRotation();
-		script.flags.insertLast(this);
-
-		//loop backwards for safe deletion
-		for(int i = attachedEntities.length()-1; i >= 0;  i--)
-		{
-			// puts("id: " + entityId);
-			entity@ ent = @entity_by_id(attachedEntities[uint(i)]);
-			if (@ent == null)
-			{
-				//this is going to happen on checkpoints if I ever add them
-				//but since stuff doesn't persist in play mode anyways it's fine
-				attachedEntities.removeAt(i);
-				continue;
-			}
-			entityHandles.insertLast(ent);
-		}
-	}
-
-	void editor_step()
-	{
-		fakeTrigger.EditorStep();
-
-		if(script.editor.editor_tab() == "Triggers" 
-			&& @script.editor.get_selected_trigger() != null
-			&& script.editor.get_selected_trigger().is_same(self.as_entity())
-			&& input.key_check_pressed_gvb(2) && input.key_check_gvb(10))
-		{
-			Vector2 mpos = Vector2(input.mouse_x_world(21), input.mouse_y_world(21));
-			for(uint i = 0; i < script.flats.length(); i++)
-			{
-				d3EnemyBase@ flat = cast<d3EnemyBase>(script.flats[i]);
-				if (flat is null || flat.depth < -flat.thickness) { continue; }
-				if (!flat.drawRect.PointInside(mpos) || flat.entity is null) { continue; }
-				uint id = flat.entity.id();
-				int ind = attachedEntities.find(id);
-				if (ind != -1)
-				{
-					attachedEntities.removeAt(ind);
-					ind = entityHandles.findByRef(flat.entity);
-					if (ind != -1) { entityHandles.removeAt(ind); }
-				}
-				else
-				{
-					entityHandles.insertLast(flat.entity);
-					attachedEntities.insertLast(id);
-				}
-				break;
-			}
-		}
-	}
-
-	void step()
-	{
-		if (ended) { return; }
-		bool end = entityHandles.length() != 0;
-		for(uint i = 0; i < entityHandles.length(); i++)
-		{
-			if (@entityHandles[i] != null && !entityHandles[i].destroyed())
-			{
-				end = false;
-				break;
-			}
-		}
-		if (end)
-		{
-			get_scene().end_level(fakeTrigger.ssp.x, fakeTrigger.ssp.y);
-			ended = true;
-		}
-	}
-
-	void editor_draw(float ok)
-	{
-		scene@ s = get_scene();
-		if(script.editor.editor_tab() == "Triggers" 
-			&& @script.editor.get_selected_trigger() != null
-			&& script.editor.get_selected_trigger().is_same(self.as_entity()))
-		{
-			for(uint i = 0; i < script.flats.length(); i++)
-			{
-				d3EnemyBase@ flat = cast<d3EnemyBase>(script.flats[i]);
-				if (flat is null || flat.depth < -flat.thickness) { continue; }
-				if (flat.entity is null || attachedEntities.find(flat.entityId) == -1) { continue; }
-				s.draw_line_world(21, 1, fakeTrigger.ssp.x, fakeTrigger.ssp.y, 
-						 flat.fakeTrigger.ssp.x, flat.fakeTrigger.ssp.y, 3, 0xFFFF0000);
-			}	
-		}
-	}
-
-	void UpdateRotation()
-	{
-		fakeTrigger.UpdateRotation();
-	}
-
-	void on_remove()
-	{
-		fakeTrigger.DeleteSelf();
-		int i = script.flags.find(this);
-		if (i != -1)
-		{
-			script.flags.removeAt(i);
 		}
 	}
 }
---
+++ map/customCollision/3dTriggers.cpp
@@ -0,0 +1,271 @@
+//for non-drawn 3d entities
+
+class d3StartPos : trigger_base
+{
+	scripttrigger@ self;
+	script@ script;
+	d3::d3Manager@ manager;
+
+	[hidden] d3FakeTrigger fakeTrigger;
+	[hidden] bool hasInit = false;
+
+	void init(script@ s, scripttrigger@ self)
+	{
+		@script = @s;
+		@manager = @s.manager;
+		@this.self = @self;
+
+		if (@s.startPos != null && !(s.startPos is this)&& !self.destroyed())
+		{
+			get_scene().remove_entity(s.startPos.self.as_entity());
+		}
+		if (!self.destroyed())
+		{
+			@s.startPos = @this;
+		}
+		if (!hasInit)
+		{
+			fakeTrigger = d3FakeTrigger();
+			fakeTrigger.colour = 0xFF00FF00;
+			hasInit = true;
+		}
+		fakeTrigger.Init(self.as_entity(), s, manager);
+		self.editor_handle_size(0);
+		UpdateRotation();
+		script.startCoords = fakeTrigger.pos;
+		s.firstFrame = true;
+	}
+
+	void editor_step()
+	{
+		fakeTrigger.EditorStep();
+		script.startCoords = fakeTrigger.pos;
+	}
+
+	void UpdateRotation()
+	{
+		fakeTrigger.UpdateRotation();
+	}
+
+	void on_remove()
+	{
+		fakeTrigger.DeleteSelf();
+	}
+}
+
+class d3EndFlag : trigger_base
+{
+	scripttrigger@ self;
+	script@ script;
+	d3::d3Manager@ manager;
+	input_api@ input;
+
+	[hidden] d3FakeTrigger fakeTrigger;
+	[hidden] bool hasInit = false;
+
+	//ids
+	[hidden] array<uint> attachedEntities;
+	array<entity@> entityHandles;
+
+	bool ended = false;
+
+	void init(script@ s, scripttrigger@ self)
+	{
+		@script = @s;
+		@manager = @s.manager;
+		@this.self = @self;
+		@input = @get_input_api();
+
+		if (!hasInit)
+		{
+			fakeTrigger = d3FakeTrigger();
+			fakeTrigger.colour = 0xFFFF0000;
+			hasInit = true;
+		}
+		fakeTrigger.Init(self.as_entity(), s, manager);
+		self.editor_handle_size(0);
+		UpdateRotation();
+		script.flags.insertLast(this);
+
+		//loop backwards for safe deletion
+		for(int i = attachedEntities.length()-1; i >= 0;  i--)
+		{
+			// puts("id: " + entityId);
+			entity@ ent = @entity_by_id(attachedEntities[uint(i)]);
+			if (@ent == null)
+			{
+				//this is going to happen on checkpoints if I ever add them
+				//but since stuff doesn't persist in play mode anyways it's fine
+				attachedEntities.removeAt(i);
+				continue;
+			}
+			entityHandles.insertLast(ent);
+		}
+	}
+
+	void editor_step()
+	{
+		fakeTrigger.EditorStep();
+
+		if(script.editor.editor_tab() == "Triggers" 
+			&& @script.editor.get_selected_trigger() != null
+			&& script.editor.get_selected_trigger().is_same(self.as_entity())
+			&& input.key_check_pressed_gvb(2) && input.key_check_gvb(10))
+		{
+			Vector2 mpos = Vector2(input.mouse_x_world(21), input.mouse_y_world(21));
+			for(uint i = 0; i < script.flats.length(); i++)
+			{
+				d3EnemyBase@ flat = cast<d3EnemyBase>(script.flats[i]);
+				if (flat is null || flat.depth < -flat.thickness) { continue; }
+				if (!flat.drawRect.PointInside(mpos) || flat.entity is null) { continue; }
+				uint id = flat.entity.id();
+				int ind = attachedEntities.find(id);
+				if (ind != -1)
+				{
+					attachedEntities.removeAt(ind);
+					ind = entityHandles.findByRef(flat.entity);
+					if (ind != -1) { entityHandles.removeAt(ind); }
+				}
+				else
+				{
+					entityHandles.insertLast(flat.entity);
+					attachedEntities.insertLast(id);
+				}
+				break;
+			}
+		}
+	}
+
+	void step()
+	{
+		if (ended) { return; }
+		bool end = entityHandles.length() != 0;
+		for(uint i = 0; i < entityHandles.length(); i++)
+		{
+			if (@entityHandles[i] != null && !entityHandles[i].destroyed())
+			{
+				end = false;
+				break;
+			}
+		}
+		if (end)
+		{
+			get_scene().end_level(fakeTrigger.ssp.x, fakeTrigger.ssp.y);
+			ended = true;
+		}
+	}
+
+	void editor_draw(float ok)
+	{
+		scene@ s = get_scene();
+		if(script.editor.editor_tab() == "Triggers" 
+			&& @script.editor.get_selected_trigger() != null
+			&& script.editor.get_selected_trigger().is_same(self.as_entity()))
+		{
+			for(uint i = 0; i < script.flats.length(); i++)
+			{
+				d3EnemyBase@ flat = cast<d3EnemyBase>(script.flats[i]);
+				if (flat is null || flat.depth < -flat.thickness) { continue; }
+				if (flat.entity is null || attachedEntities.find(flat.entityId) == -1) { continue; }
+				s.draw_line_world(21, 1, fakeTrigger.ssp.x, fakeTrigger.ssp.y, 
+						 flat.fakeTrigger.ssp.x, flat.fakeTrigger.ssp.y, 3, 0xFFFF0000);
+			}	
+		}
+	}
+
+	void UpdateRotation()
+	{
+		fakeTrigger.UpdateRotation();
+	}
+
+	void on_remove()
+	{
+		fakeTrigger.DeleteSelf();
+		int i = script.flags.find(this);
+		if (i != -1)
+		{
+			script.flags.removeAt(i);
+		}
+	}
+}
+
+class d3TextTrigger : trigger_base
+{
+	scripttrigger@ self;
+	script@ script;
+	d3::d3Manager@ manager;
+
+	[hidden] d3FakeTrigger fakeTrigger;
+	[hidden] bool hasInit = false;
+	//no you don't get a visualisation fuck you deal with it
+	[text] float range = 1000;
+
+	[hidden] int ttId = -1;
+	entity@ tt;
+
+	void init(script@ s, scripttrigger@ self)
+	{
+		@script = @s;
+		@manager = @s.manager;
+		@this.self = @self;
+
+		if (!hasInit)
+		{
+			fakeTrigger = d3FakeTrigger();
+			fakeTrigger.colour = 0xFFFFFF00;
+			hasInit = true;
+		}
+		fakeTrigger.Init(self.as_entity(), s, manager);
+		self.editor_handle_size(0);
+		UpdateRotation();
+
+		if (ttId == -1)
+		{
+			@tt = create_entity("text_trigger");
+			get_scene().add_entity(tt);
+			ttId = tt.id();
+		}
+		else
+		{
+			@tt = @entity_by_id(uint(ttId));
+			if (@tt == null)
+			{
+				@tt = create_entity("text_trigger");
+				get_scene().add_entity(tt);
+				ttId = tt.id();
+			}
+		}
+	}
+
+	void editor_step()
+	{
+		fakeTrigger.EditorStep();
+		step();
+	}
+
+	void step()
+	{
+		Vector2 camCen = manager.cam.igCoords;
+		if ((manager.cam.centre - fakeTrigger.pos).Magnitude() < range)
+		{
+			tt.x(camCen.x);
+			tt.y(camCen.y);
+		}
+		else
+		{
+			tt.x(camCen.x);
+			tt.y(camCen.y + 5000);
+		}
+	}
+
+	void UpdateRotation()
+	{
+		fakeTrigger.UpdateRotation();
+	}
+
+	void on_remove()
+	{
+		fakeTrigger.DeleteSelf();
+		get_scene().remove_entity(tt);
+	}
+}
